7cff78497d18ac9cb7606cdd59a3ed2aee0a10b6
Scott Martin <scott.martin88@live.com>
2023-12-21 11:38:15 +0100

    Build GenreDropdown component, making API request with caching to TMDB. Typings set

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggler } from "./ThemeToggler";
 import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
 
 function Header() {
   return (
@@ -17,6 +18,7 @@ function Header() {
       </Link>
 
       <div className="flex space-x-2">
+        <GenreDropdown />
         <SearchInput />
         <ThemeToggler />
       </div>
