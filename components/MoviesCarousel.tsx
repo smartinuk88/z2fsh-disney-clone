2ee0dc6996090a0d732db2a6b84ed8514b0fa900
Scott Martin <scott.martin88@live.com>
2023-12-21 16:23:21 +0100

    Modify structure and styling for isVertical in MovieCarousel

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -11,16 +11,34 @@ type Props = {
 function MoviesCarousel({ title, movies, isVertical }: Props) {
   return (
     <div className="z-50">
-      <h2>{title}</h2>
+      <h2 className="text-xl font-bold px-10 py-2">{title}</h2>
 
       <div
         className={cn(
-          "flex overflow-scroll space-x-4 px-5 lg:px-10 py-5 scrollbar-hide"
+          "flex overflow-scroll space-x-4 px-5 lg:px-10 py-5 scrollbar-hide",
+          isVertical && "flex-col space-x-0 space-y-12"
         )}
       >
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {isVertical
+          ? movies.map((movie) => (
+              <div
+                key={movie.id}
+                className={cn(
+                  isVertical &&
+                    "flex flex-col space-y-5 mb-5 items-center lg:flex-row space-x-5"
+                )}
+              >
+                <MovieCard movie={movie} />
+                <div className="max-w-2xl">
+                  <p className="font-bold">
+                    {movie.title} ({movie.release_date?.split("-")[0]})
+                  </p>
+                  <hr className="mb-3" />
+                  <p>{movie.overview}</p>
+                </div>
+              </div>
+            ))
+          : movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
