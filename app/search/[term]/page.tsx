e0409a15bde05dbea41b76fcafe2063ae2a2f370
Scott Martin <scott.martin88@live.com>
2023-12-22 22:07:12 +0100

    Build search page with popular movie carousel

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -6,16 +8,23 @@ type Props = {
   };
 };
 
-function SearchPage({ params: { term } }: Props) {
+async function SearchPage({ params: { term } }: Props) {
   if (!term) notFound();
 
   const termToUse = decodeURI(term);
 
-  // API call to get searched movies
+  const movies = await getSearchedMovies(termToUse);
+  const popularMovies = await getPopularMovies();
 
-  // API call to get popular movies
-
-  return <div>page</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+        <MoviesCarousel title="You may also like" movies={popularMovies} />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
